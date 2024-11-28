f2d782d7c119f75e2715c2d6d91d0aadd127070f
Vinicius Gabriel Ochner <vinicius.ochner@fulltechpro.com.br>
2024-11-27 21:01:42 -0300

    alterando url mapi

--- secbank-web/src/api/axiosBase.js
+++ secbank-web/src/api/axiosBase.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with a centralized base URL
 const api = axios.create({
-  baseURL: 'https://secbank-lb-1516101896.us-east-1.elb.amazonaws.com', // replace with your API base URL
+  baseURL: 'https://secbank.vinilab.dev', // replace with your API base URL
   headers: {
     'Content-Type': 'application/json', // default headers (optional)
     // Add any other headers you need, e.g., Authorization
