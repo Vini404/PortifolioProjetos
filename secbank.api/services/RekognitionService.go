0613f739b8e70ec9231b0f34fafb4e67c2a5fd10
Vinicius Gabriel Ochner <vinicius.ochner@fulltechpro.com.br>
2024-12-01 11:54:28 -0300

    refactor rekognition service

--- secbank.api/services/RekognitionService.go
+++ secbank.api/services/RekognitionService.go
@@ -10,48 +10,39 @@ import (
 	"strconv"
 )
 
-// RekognitionService agora aceita a interface RekognitionAPI
+// RekognitionService encapsula operações do Rekognition
 type RekognitionService struct {
 	Client rekognitioniface.RekognitionAPI
 }
 
-// Função para inicializar o RekognitionService
+// Inicializa RekognitionService com uma sessão AWS
 func NewRekognitionService(region string) *RekognitionService {
 	sess := session.Must(session.NewSession(&aws.Config{
 		Region:      aws.String(region),
 		Credentials: credentials.NewEnvCredentials(),
 	}))
-
-	return &RekognitionService{
-		Client: rekognition.New(sess),
-	}
+	return &RekognitionService{Client: rekognition.New(sess)}
 }
 
-// Método para criar uma coleção
+// Cria uma coleção e retorna seu ID
 func (r *RekognitionService) CreateCollection() (string, error) {
 	collectionID := uuid.New().String()
 	_, err := r.Client.CreateCollection(&rekognition.CreateCollectionInput{
 		CollectionId: aws.String(collectionID),
 	})
-	if err != nil {
-		return "", err
-	}
-	return collectionID, nil
+	return collectionID, err
 }
 
-// Método para criar um usuário
+// Cria um usuário em uma coleção
 func (r *RekognitionService) CreateUser(collectionID string, customerID int) error {
 	_, err := r.Client.CreateUser(&rekognition.CreateUserInput{
 		CollectionId: aws.String(collectionID),
 		UserId:       aws.String(strconv.Itoa(customerID)),
 	})
-	if err != nil {
-		return err
-	}
-	return nil
+	return err
 }
 
-// Método para associar faces a um usuário
+// Associa faces a um usuário
 func (r *RekognitionService) AssociateFacesToUser(collectionID string, userID int, faceIDs []string) error {
 	_, err := r.Client.AssociateFaces(&rekognition.AssociateFacesInput{
 		CollectionId: aws.String(collectionID),
@@ -61,45 +52,29 @@ func (r *RekognitionService) AssociateFacesToUser(collectionID string, userID in
 	return err
 }
 
-// Método para indexar faces a partir de uma imagem codificada em base64
+// Indexa faces em uma coleção
 func (r *RekognitionService) IndexFaces(collectionID string, imageBytes []byte) (*rekognition.IndexFacesOutput, error) {
-	image := &rekognition.Image{
-		Bytes: imageBytes,
-	}
-	response, err := r.Client.IndexFaces(&rekognition.IndexFacesInput{
+	return r.Client.IndexFaces(&rekognition.IndexFacesInput{
 		CollectionId: aws.String(collectionID),
-		Image:        image,
+		Image:        &rekognition.Image{Bytes: imageBytes},
 	})
-	if err != nil {
-		return nil, err
-	}
-	return response, nil
 }
 
-// Método para buscar usuários por uma imagem codificada em base64
+// Busca usuários por uma imagem
 func (r *RekognitionService) SearchUsersByImage(collectionID string, imageBytes []byte) (*rekognition.SearchUsersByImageOutput, error) {
-	image := &rekognition.Image{
-		Bytes: imageBytes,
-	}
-	response, err := r.Client.SearchUsersByImage(&rekognition.SearchUsersByImageInput{
+	return r.Client.SearchUsersByImage(&rekognition.SearchUsersByImageInput{
 		CollectionId: aws.String(collectionID),
-		Image:        image,
+		Image:        &rekognition.Image{Bytes: imageBytes},
 	})
-	if err != nil {
-		return nil, err
-	}
-	return response, nil
 }
 
-// Método para obter os IDs das faces indexadas
-func (r *RekognitionService) GetFacesIDs(responseToIndexFace *rekognition.IndexFacesOutput) []string {
-	var faceIDs []string
-
-	for _, faceRecord := range responseToIndexFace.FaceRecords {
+// Obtém os IDs das faces indexadas
+func (r *RekognitionService) GetFacesIDs(indexOutput *rekognition.IndexFacesOutput) []string {
+	faceIDs := make([]string, len(indexOutput.FaceRecords))
+	for i, faceRecord := range indexOutput.FaceRecords {
 		if faceRecord.Face != nil && faceRecord.Face.FaceId != nil {
-			faceIDs = append(faceIDs, *faceRecord.Face.FaceId)
+			faceIDs[i] = *faceRecord.Face.FaceId
 		}
 	}
-
 	return faceIDs
 }
